4f606e2863e4220f18e8b2730f2c84da0626606a
Mandi2000 <armanditov2000@gmail.com>
2023-03-24 13:25:37 +0100

    ¨cambios¨

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
-import "./App.css";
-import { productMenu } from "./components/Products/data";
 import { Footer } from "./components/Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from "./components/Navbar";
 import { Home } from "./components/Home";
 import RestaurantMenu from "./components/Menu";
 
+import { useMemo } from "react";
+import { productMenu } from "./components/Products/data";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
+  const memoizedMenuItems = useMemo(() => productMenu, []);
+
   return (
     <Router>
-    <MyNavBar/>
-    <GlobalStyle/>
-    <Home />
-    <RestaurantMenu menuItems={productMenu} title=""/> 
-    <Footer/>
-  </Router>
-
+      <MyNavBar />
+      <GlobalStyle />
+      <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/menu" element={            
+          <RestaurantMenu menuItems={memoizedMenuItems} title="" />}
+          />
+        </Routes>
+        <Footer />
+    </Router>
   );
 }
 
 export default App;
+
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,22 +13,22 @@ function MyNavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="#" onClick={scrollToTop}>PIZZA</Navbar.Brand>
+        <Navbar.Brand href="/" onClick={scrollToTop}>PIZZA</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#favorite">Favoritos</Nav.Link>
-            <Nav.Link href="#sweets">Postres</Nav.Link>
+            <Nav.Link href="/#favorite">Favoritos</Nav.Link>
+            <Nav.Link href="/#sweets">Postres</Nav.Link>
             <NavDropdown title="Info" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#time">Horarios</NavDropdown.Item>
-              <NavDropdown.Item href="#about">
+              <NavDropdown.Item href="/#time">Horarios</NavDropdown.Item>
+              <NavDropdown.Item href="/#about">
               About Us
               </NavDropdown.Item>
-              <NavDropdown.Item href="#location">
+              <NavDropdown.Item href="/#location">
               Localización
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#menu">
+              <NavDropdown.Item href="/menu">
               Menu
               </NavDropdown.Item>
             </NavDropdown>
